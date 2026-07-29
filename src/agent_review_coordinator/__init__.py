755c92a4f353e9777dead5cc5534a8ce6082a0a2
Ilya Ganelin <ilganeli@gmail.com>
2026-07-28 22:17:55 -0700

    feat: model provider-neutral review policy

--- src/agent_review_coordinator/__init__.py
+++ src/agent_review_coordinator/__init__.py
@@ -1,3 +1,13 @@
 """Provider-neutral review coordination and settlement."""
 
+from .policy import ReviewPolicy, ReviewerSlot, ReviewStage, ReviewStagePolicy
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "ReviewPolicy",
+    "ReviewStage",
+    "ReviewStagePolicy",
+    "ReviewerSlot",
+    "__version__",
+]
